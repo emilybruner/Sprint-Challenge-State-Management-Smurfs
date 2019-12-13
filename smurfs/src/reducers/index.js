1f196c8fa68e1ec4d7686697d54944fb4d521ef8
Emily <bruner.emilym@gmail.com>
2019-12-13 11:26:42 -0500

    wrote out reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,3 +1,5 @@
+import {FETCHING, FETCHING_SUCCESS, FETCHING_FAILED, ADD_SMURF, ADD_SMURF_SUCCESS, ADD_SMURF_FAILED} from '../actions/index';
+
 const initialState = {
     smurfs: [],
     fetchingSmurf: false,
@@ -8,6 +10,47 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCHING:
+            return {
+                ...state,
+                fetchingSmurf: false,
+                error: ''
+            }
+        case FETCHING_SUCESS:
+            return {
+                ...state, 
+                fetchingSmurf: true,
+                error: '',
+                smurfs: [...state.smurfs, ...action.payload]
+            }
+        case FETCHING_FAILED:
+            return {
+                ...state,
+                fetchingSmurf: false,
+                error: action.payload
+            }
+        case ADD_SMURF:
+            return {
+                ...state,
+                fetchingSmurf: false,
+                addingSmurf: true,
+                error: ''
+            }
+        case ADD_SMURF_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                fetchingSmurfs: false,
+                addingSmurf: false,
+                error: ''
+            }
+        case ADD_SMURF_FAILED:
+            return {
+                ...state,
+                fetchingSmurfs: false,
+                addingSmurf: false,
+                error: ''
+            }
         default: 
         return state;
     }
